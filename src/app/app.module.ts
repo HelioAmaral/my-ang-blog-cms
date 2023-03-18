c54d295d79551e0e7d35fc6ae0137a8700c52ac0
HelioAmaral <helio.amaral81@gmail.com>
2023-03-18 16:45:00 +0100

    first change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { CategoriesComponent } from './categories/categories.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'my-blog-bcknd',
+    appId: '1:712531227639:web:7c478b6b2a1e09d96974f2',
+    storageBucket: 'my-blog-bcknd.appspot.com',
+    apiKey: 'AIzaSyCmjmFGsg6GUwkCSUrHgX633ol2gZ3_SCU',
+    authDomain: 'my-blog-bcknd.firebaseapp.com',
+    messagingSenderId: '712531227639',
+  },
+    production: false
+}
